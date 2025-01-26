e00410b7e02257746ecbe4aabaa226f82a1de44c
karin <karin@DESKTOP-JFTOVL2>
2025-01-26 15:44:35 +0100

    Add Javadoc comments to `Palindrom` and `meineListe` classes
    - Add Javadoc to `isPalindrom` method in `Palindrom` class
    - Add Javadoc to `mergeAndSortLists` method in `meineListe`

--- src/main/java/a/Palindrom.java
+++ src/main/java/a/Palindrom.java
@@ -2,6 +2,12 @@ package a;
 
 public class Palindrom {
 
+	/**
+	 * Methode, die überprüft, ob ein String ein Palindrom ist.
+	 * 
+	 * @param string
+	 * @return true, wenn der String ein Palindrom ist, sonst false
+     */
 	public boolean isPalindrom(String string) {
 		char [] charArray = string.toCharArray();
 		for (int i = 0; i < charArray.length / 2; i++) {
--- src/main/java/a/meineListe.java
+++ src/main/java/a/meineListe.java
@@ -7,6 +7,13 @@ import java.util.List;
 import java.util.Set;
 
 public class meineListe {
+	
+	/**
+	 * Methode, die zwei Listen zusammenführt, duplizierte Elemente entfernt und die Elemente sortiert zurückgibt.
+	 * @param list1
+	 * @param list2
+	 * @return sortierte zusmammengefügte Liste ohne Duplikate
+	 */
     public List<Integer> mergeAndSortLists(List<Integer> list1, List<Integer> list2) {
         Set<Integer> mergedSet = new HashSet<>(list1);
         mergedSet.addAll(list2);
