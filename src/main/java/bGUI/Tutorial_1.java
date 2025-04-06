5a19e15f45954c30214b13d8538680441a85342e
karin <karin@DESKTOP-JFTOVL2>
2025-04-06 19:50:11 +0200

    GUI

---
+++ src/main/java/bGUI/Tutorial_1.java
@@ -0,0 +1,14 @@
+package bGUI;
+
+public class Tutorial_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AppWindow theAppWindow = new AppWindow();
+		theAppWindow.setBounds(10, 10, 420, 180);
+		theAppWindow.show();
+	}
+
+}
